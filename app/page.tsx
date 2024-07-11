ef0b46f816dd7299e7cbd4b562261d53e05a2e29
Aaryan Prothi <aaryanprothi.m37a@gmail.com>
2024-07-11 19:53:57 +0530

    experience section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -11,13 +12,12 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
--- components/ui/InfiniteMovingCards.tsx
+++ components/ui/InfiniteMovingCards.tsx
@@ -109,10 +109,10 @@ export const InfiniteMovingCards = ({
                     <img src="/profile.svg" alt="profile" />
                   </div>
                   <div className="flex flex-col gap-1">
-                    <span className=" text-sm leading-[1.6] text-white font-bold">
+                    <span className=" text-xl leading-[1.6] text-white font-bold">
                       {item.name}
                     </span>
-                    <span className=" text-xl leading-[1.6] text-white-200 font-normal">
+                    <span className=" text-sm leading-[1.6] text-white-200 font-normal">
                       {item.title}
                     </span>
                   </div>
