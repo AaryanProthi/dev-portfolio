652bdd736efdefb86e1fa9d521d380a668bab14b
Aaryan Prothi <aaryanprothi.m37a@gmail.com>
2024-07-16 11:37:53 +0530

    favicon added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
@@ -13,8 +11,6 @@ import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
-
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
@@ -30,4 +26,3 @@ export default function Home() {
     </main>
   );
 }
-
